cd53a6c35ac2a8fe4d580058909efb3c2d1b1da6
Justin Morris <justinjodymorris@gmail.com>
2019-08-17 23:08:12 -0700

    fix: header test

--- src/__test__/components/header/header.test.js
+++ src/__test__/components/header/header.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import Header from '../../../components/header/header'; 
+import LeaveARequest from '../../../components/leaveARequest/leaveARequest';
 
 describe('Header Component', () => {
   test('Should Render Header Component', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.debug()).toMatchSnapshot();
+  });
+
+  test('Should render LeaveARequest Component', () => {
+    const wrapper = shallow(<Header />);
+    const component = wrapper.find(LeaveARequest);
+    expect(component).toBeDefined();
   });
 });
